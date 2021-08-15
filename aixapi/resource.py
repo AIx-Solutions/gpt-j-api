c2dd76b4c85f0c05b3ab8e6351568b160eaf5775
Josh Wolff <jw1@stanford.edu>
2021-08-15 11:48:45 -0700

    Updating parameters - token_min_length, token_max_length, top_k, custom_model_id

--- aixapi/resource.py
+++ aixapi/resource.py
@@ -69,48 +69,66 @@ class AIxResource:
     def compose(
         self,
         prompt: str,
-        response_length: int = 64,
+        token_min_length: int = 64,
+        token_max_length: int = 512,
         temperature: float = 0.7,
         top_p: float = 1.0,
-        stop_sequence: str = str()
+        top_k: int = 50,
+        stop_sequence: str = str(),
+        custom_model_id: str = None
     ):
         """
         Sends a prompt to be completed by the GPT engine.
 
         :param prompt: A string that prompts the AI. The AI will complete it.
-        :param response_length: Valid values 64 to 2048. The maximum number of tokens to generate. Requests can use up
+        :param token_min_length: Valid values 64 to 2048. The minimum number of tokens to generate. Requests can use up
+        to 2048 tokens shared between prompt and completion. (One token is roughly 4 characters for normal English
+        text.)
+        :param token_max_length: Valid values 64 to 2048. The maximum number of tokens to generate. Requests can use up
         to 2048 tokens shared between prompt and completion. (One token is roughly 4 characters for normal English
         text.)
         :param temperature: Valid values 0.00 to 1.00. Controls randomness. Lowering results in less random completions.
         As the temperature approaches zero, the model will become deterministic and repetitive.
         :param top_p: Valid values 0.00 to 1.00. Controls diversity via nucleus sampling: 0.5 means half of all
         likelihood-weighted options are considered.
+        :param top_k: Valid values 0 to 50 (integer). Controls the number of highest probability vocabulary
+        tokens to keep for top-k-filtering.
         :param stop_sequence: A sequence where the API will stop generating further tokens. The returned text will not
         contain the stop sequence.
+        :param custom_model_id: A custom model ID to provide if you are accessing a custom or private model.
+        If this is not provided, the default GPT-J is used.
         :return:
             :compute_time: The time taken to compute the response
-            :model: GPT-J-6B (Will vary in the future when we add more NLP models)
+            :model: Name of the model used e.g., GPT-J-6B
             :text: The response by the AI
             :prompt: The value you provided
+            :token_min_length: The value you provided
             :token_max_length: The value you provided
             :temperature: The value you provided
             :top_p: The value you provided
+            :top_k: The value you provided
             :stop_sequence: The value you provided
 
         """
-        assert type(response_length) is int
+        assert type(token_min_length) is int
+        assert type(token_max_length) is int
         assert type(temperature) is float
         assert type(top_p) is float
+        assert type(top_k) is int
         assert type(prompt) is str
         assert type(stop_sequence) is str
+        assert type(custom_model_id) is str
 
         return self._request(
             payload={
                 "prompt": prompt,
-                "response_length": response_length,
+                "token_min_length": token_min_length,
+                "token_max_length": token_max_length,
                 "temperature": temperature,
                 "top_p": top_p,
-                "stop_sequence": stop_sequence
+                "top_k": top_k,
+                "stop_sequence": stop_sequence,
+                "custom_model_id": custom_model_id
             },
             endpoint="compose",
             request_method=self.RequestMethod.POST
